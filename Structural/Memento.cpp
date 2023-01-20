55530263fa11ae17dc7f94697ddf9802466d6ea0
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2023-01-20 08:06:53 +0100

    Update Memento.cpp

--- Structural/Memento.cpp
+++ Structural/Memento.cpp
@@ -2,15 +2,19 @@
 #include <vector>
 #include <string>
 #include <queue>
+#include <memory>
+
 using namespace std;
 
-class Momento
+class Memento
 {
 	int balance;
 public:
-	Momento(int b) :balance(b) {}
+	Memento(int b) :balance(b) {}
 
 	friend class BankAccount;
+	friend class BankAccount2;
+
 };
 
 class BankAccount
@@ -20,12 +24,12 @@ class BankAccount
 public:
 	BankAccount(int balance) :balance(balance) {}
 
-	Momento deposit(int amount) {
+	Memento deposit(int amount) {
 		balance += amount;
 		return {balance};
 	}
 
-	void restore(const Momento& m)
+	void restore(const Memento& m)
 	{
 		balance = m.balance;
 	}
@@ -38,6 +42,68 @@ public:
 
 };
 
+class BankAccount2
+{
+	int balance{ 0 };
+	vector<shared_ptr<Memento>> changes;
+	int current;
+
+public:
+	BankAccount2(int balance) :balance(balance)
+	{
+		changes.emplace_back(make_shared<Memento>(balance));
+		current = 0;
+	}
+
+	shared_ptr<Memento> deposit(int amount)
+	{
+		balance += amount;
+		auto m = make_shared<Memento>(balance);
+		changes.push_back(m);
+		current++;
+		return m;
+	}
+
+	void restore(const shared_ptr<Memento>& m)
+	{
+		if (m)
+		{
+			balance = m->balance;
+			changes.push_back(m);
+			current = changes.size() - 1;
+			
+		}
+	}
+
+	shared_ptr<Memento> undo()
+	{
+		if (current > 0)
+		{
+			--current;
+			auto m = changes[current];
+			balance = m->balance;
+		}
+		return {};
+	}
+
+	shared_ptr<Memento> redo()
+	{
+		if (current + 1 < changes.size())
+		{
+			++current;
+			auto m = changes[current];
+			balance = m->balance;
+			return m;
+		}
+		return {};
+	}
+
+	friend ostream& operator<<(ostream& os, const BankAccount2& acc)
+	{
+		os << "balance: " << acc.balance;
+		return os;
+	}
+};
 
 
 int main()
@@ -53,6 +119,20 @@ int main()
 	cout<<"back to m1: " << ba << "\n";
 	ba.restore(m2);
 	cout << "back to m1: " << ba << "\n";
+
+	BankAccount2 ba2{ 100 };
+	ba2.deposit(50);
+	ba2.deposit(20);
+	cout << ba2 << endl;
+
+	ba2.undo();
+	cout<<"undo 1: " << ba2 << endl;
+	
+	ba2.undo();
+	cout << "undo 2: " << ba2 << endl;
+
+	ba2.redo();
+	cout << "redo: " << ba2 << endl;
 
 	
 
