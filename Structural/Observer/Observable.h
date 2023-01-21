35aef020d50c7ea1aacce1d3c9b6ec0c08df6c0c
Omar Hosny <omarhosny.barcelona@gmail.com>
2023-01-21 12:23:20 +0100

    observer

---
+++ Structural/Observer/Observable.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "Observer.h"
+using namespace std;
+
+
+template<typename T>
+struct Observable
+{
+	vector<Observer<T>*> observers;
+
+	void notify(T& source, const string& field_name)
+	{
+		for (auto observer : observers)
+		{
+			observer->field_changed(source, field_name);
+		}
+
+		void subscribe(Observer<T> &observer)
+		{
+			observers.push_back(observer);
+		}
+
+		void unsubscribe(Observer<T>& observer)
+		{
+			observers.erase(observer);
+		}
+	}
+};
+
