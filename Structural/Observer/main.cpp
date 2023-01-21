5aea4284ef6e7ed10f67f591dbc330a6f5d1040c
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2023-01-21 13:05:23 +0100

    Update main.cpp

--- Structural/Observer/main.cpp
+++ Structural/Observer/main.cpp
@@ -3,137 +3,97 @@
 #include <string>
 #include <queue>
 #include <memory>
+#include "Observer.h"
+#include "Observable.h"
+#include <boost/signals2.hpp>
+
+using namespace boost;
+using namespace boost::signals2;
 
 using namespace std;
 
-class Memento
+
+class Person : public Observable<Person>//observable
 {
-	int balance;
+	int age;
 public:
-	Memento(int b) :balance(b) {}
-
-	friend class BankAccount;
-	friend class BankAccount2;
-
-};
-
-class BankAccount
-{
-	int balance{ 0 };
-
-public:
-	BankAccount(int balance) :balance(balance) {}
-
-	Memento deposit(int amount) {
-		balance += amount;
-		return {balance};
+	Person(int a) {
+		age = a;
 	}
 
-	void restore(const Memento& m)
-	{
-		balance = m.balance;
+	int getAge() {
+		return age;
 	}
 
-	friend ostream& operator<<(ostream& os, const BankAccount& acc)
-	{
-		os << "balance: " << acc.balance;
-		return os;
+	void setAge(int age) {
+		if (this->age == age)return;
+
+		this->age = age;
+		notify(*this, "age");
 	}
 
 };
 
-class BankAccount2
+//observer & observable
+struct ConsolePersonObserver: public Observer<Person>
 {
-	int balance{ 0 };
-	vector<shared_ptr<Memento>> changes;
-	int current;
+private:
+	void field_changed(Person& source, const string& field_name) override {
+		cout << "Person's " << field_name << " has changed to ";
+		if (field_name == "age") cout << source.getAge();
+		cout << "\n";
+	}
 
+};
+
+template<typename T> 
+struct Observable2
+{
+	signal<void(T&, const string&)> field_changed;
+	
+};
+
+class Person2 : public Observable2<Person2>
+{
+	int age;
 public:
-	BankAccount2(int balance) :balance(balance)
-	{
-		changes.emplace_back(make_shared<Memento>(balance));
-		current = 0;
+	int get_age()const {
+		return age;
 	}
 
-	shared_ptr<Memento> deposit(int amount)
+	void set_age(int age)
 	{
-		balance += amount;
-		auto m = make_shared<Memento>(balance);
-		changes.push_back(m);
-		current++;
-		return m;
-	}
+		if (this->age == age)return;
+		this->age = age;
+		field_changed(*this, "age");
 
-	void restore(const shared_ptr<Memento>& m)
-	{
-		if (m)
-		{
-			balance = m->balance;
-			changes.push_back(m);
-			current = changes.size() - 1;
-			
-		}
-	}
-
-	shared_ptr<Memento> undo()
-	{
-		if (current > 0)
-		{
-			--current;
-			auto m = changes[current];
-			balance = m->balance;
-		}
-		return {};
-	}
-
-	shared_ptr<Memento> redo()
-	{
-		if (current + 1 < changes.size())
-		{
-			++current;
-			auto m = changes[current];
-			balance = m->balance;
-			return m;
-		}
-		return {};
-	}
-
-	friend ostream& operator<<(ostream& os, const BankAccount2& acc)
-	{
-		os << "balance: " << acc.balance;
-		return os;
 	}
 };
 
 
 int main()
 {
+	Person person(10);
+	ConsolePersonObserver cpo;
+
+	person.subscribe(cpo);
 	
-	BankAccount ba{ 100 };
-	auto m1 = ba.deposit(50);
-	auto m2 = ba.deposit(25);
+	person.setAge(11);
+	person.setAge(12);
+	person.unsubscribe(cpo);
+	person.setAge(13);
 
-	cout << ba << "\n";
+	Person2 person2;
+	auto connect = person2.field_changed.connect(
+		[](Person2& p, const string& field_name) {
+			cout << field_name << " has changed\n";
+		}
+	);
 
-	ba.restore(m1);
-	cout<<"back to m1: " << ba << "\n";
-	ba.restore(m2);
-	cout << "back to m1: " << ba << "\n";
+	person2.set_age(20);
+	person2.set_age(30);
 
-	BankAccount2 ba2{ 100 };
-	ba2.deposit(50);
-	ba2.deposit(20);
-	cout << ba2 << endl;
+	connect.disconnect(); //equivalent to unsubscribing
 
-	ba2.undo();
-	cout<<"undo 1: " << ba2 << endl;
-	
-	ba2.undo();
-	cout << "undo 2: " << ba2 << endl;
-
-	ba2.redo();
-	cout << "redo: " << ba2 << endl;
-
-	
 
 }
